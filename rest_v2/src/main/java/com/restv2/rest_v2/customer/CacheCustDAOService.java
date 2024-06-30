4b9420e2394b350072d1b3f2cd35da00d14e3217
yxk9640 <yxk9640@mavs.uta.edu>
2024-06-30 01:29:49 -0700

    Update

--- rest_v2/src/main/java/com/restv2/rest_v2/customer/CacheCustDAOService.java
+++ rest_v2/src/main/java/com/restv2/rest_v2/customer/CacheCustDAOService.java
@@ -12,59 +12,6 @@ import java.util.List;
 // Also called as Repository
 @Repository
 public interface CacheCustDAOService extends JpaRepository<Customer, Integer> {
-    // Store data in DB -> sql <- h2
 
-    //access
-//    private static List<Customer> customers = new ArrayList<>();
-//    private static int custCount = 3;
-//
-//    static {
-//        customers.add(new Customer(1, "Harish", new Date(), "pharish"));
-//        customers.add(new Customer(2, "Yogesh", new Date(),"pyogesh"));
-//        customers.add(new Customer(3, "Rish", new Date(), "prish"));
-//    }
-//
-//    // primary key done by backend
-//
-//
-//    // FindAll()
-//    public  List<Customer> findAll(){
-//
-//        return customers; //local cache
-//    }
-//
-//    // save cust <- customer
-//    public Customer save(Customer cust_new){
-//        if ( cust_new.getId() == null)
-//        {
-//            cust_new.setId(++custCount);
-//        }
-//        customers.add(cust_new);
-//        return cust_new;
-//    }
-//
-//    //find one
-//    public Customer findOne(int id){
-//        for ( Customer cust: customers){
-//            if ( cust.getId() == id)
-//                return cust;
-//        }
-//       return null;
-//    }
-//
-//    public Customer deleteById(int id){
-//
-////        cannot modify the list while searching so
-////        we need one more array or we can use iterator
-//        Iterator<Customer> iterator = customers.iterator();
-//        while ( iterator.hasNext()){
-//            Customer customer = iterator.next();
-//            if (customer.getId() == id){
-//                iterator.remove();
-//                return customer;
-//            }
-//
-//        }
-//        return null;
 
 }
